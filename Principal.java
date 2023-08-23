6c10ddcbe80059524fbd59f15a51c49e9ef759f2
daniel coila <danielcoilacenteno@gmail.com>
2023-08-22 23:10:05 -0500

    Clase Principal con …..

---
+++ Principal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Principal {
+
+
+	public class ejemplo {
+
+	    public static void main(String[] args){
+
+	        int vida, rp;
+	        boolean fl = false;
+	        vida = 0; 
+	        try (Scanner sc = new Scanner(System.in)) {
+				do {
+					vida++;
+					System.out.println("vida:"+vida+"\t¿cual es el numero secreto?");
+					rp = sc.nextInt();
+					if (rp == 1234){
+						
+						fl = true;
+					}
+					
+				}while (vida < 3 && !fl);
+			}
+	        if (fl == true) {
+	        	System.out.println("adivinaste!!!!!");
+	        }
+	        else {
+	        	System.out.println("ups, perdiste......");
+	        }
+	    }
+
+	}
+}
